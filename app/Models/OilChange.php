d79cf1bc536085bd4f54b28e8e01c2593b8787d9
Theo Wu <theowu@Theos-MacBook-Air.local>
2026-05-04 12:02:12 -0400

    Fix mass assignment by adding fillable fields

--- app/Models/OilChange.php
+++ app/Models/OilChange.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class OilChange extends Model
 {
-    //
+    protected $fillable = [
+        'current_odometer',
+        'previous_odometer',
+        'previous_change_date',
+    ];
 }
