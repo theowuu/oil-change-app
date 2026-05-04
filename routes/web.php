a37b04687366dde5a3a2cf0b0558dd2e2c745021
Theo Wu <theowu@Theos-MacBook-Air.local>
2026-05-04 12:06:17 -0400

    Update routes configuration

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\OilChangeController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [OilChangeController::class, 'index']);
+Route::post('/check', [OilChangeController::class, 'check']);
+Route::get('/result/{id}', [OilChangeController::class, 'result']);
